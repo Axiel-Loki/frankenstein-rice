30f3072632d26a24e1058fb19ee74285cc0d93dc
Maxiel Sousa <jhn.nyx0@gmail.com>
2025-12-15 14:28:39 -0300

    up7

---
+++ dotfiles/.config/fish/config.fish
@@ -0,0 +1,6 @@
+set -g fish_greeting
+set -gx GPG_TTY $(tty)
+if status is-interactive
+    set -U fish_prompt_pwd_dir_length 0
+    # Commands to run in interactive sessions can go here
+end
--- dotfiles/.config/waybar/scripts/brightness_bar.sh
+++ dotfiles/.config/waybar/scripts/brightness_bar.sh
@@ -1,9 +1,48 @@
 #!/usr/bin/env bash
 
 # Pega brilho atual e máximo
-CURRENT=$(brightnessctl get)
-MAX=$(brightnessctl max)
+# CURRENT=$(brightnessctl get)
+# MAX=$(brightnessctl max)
+# 
+# PERCENT=$(( CURRENT * 100 / MAX ))
+# 
+# echo "BRIL: $PERCENT"
 
-PERCENT=$(( CURRENT * 100 / MAX ))
+# New Script #
 
-echo "BRIL: $PERCENT"
+#!/bin/bash
+
+# Lista dispositivos de brilho disponíveis
+# brightnessctl -l
+
+# Para Intel (comum)
+device="intel_backlight"
+
+# Para laptops
+# device="acpi_video0"
+
+# Tenta diferentes dispositivos
+if brightnessctl -d $device info > /dev/null 2>&1; then
+    current=$(brightnessctl -d $device get)
+    max=$(brightnessctl -d $device max)
+else
+    # Tenta dispositivo genérico
+    current=$(brightnessctl get 2>/dev/null || echo 0)
+    max=$(brightnessctl max 2>/dev/null || echo 100)
+fi
+
+if [ "$max" -eq 0 ]; then
+    max=100
+fi
+
+percent=$((current * 100 / max))
+
+if [ "$percent" -lt 34 ]; then
+    icon="󰃞"
+elif [ "$percent" -lt 67 ]; then
+    icon="󰃟"
+else
+    icon="󰃠"
+fi
+
+echo "{\"text\":\"$icon\", \"percentage\":$percent, \"tooltip\":\"Brilho: $percent%\"}"
